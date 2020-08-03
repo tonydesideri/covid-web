9d759566fe2c38918cf415e7abef602ffe23a9d1
Tony Desideri <tony.desideri@grupoicts.com.br>
2020-08-03 09:00:13 -0400

    [FIX] - senha

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get('/login', (req, res) => {
 })
 
 app.post('/registros', (req, res) => {
-      if (req.body.senha == 'N?7DXfcG!+2+T+_ET8MRN+BSTmXpW%*$8e3aQ*kN9Yt+wQw$ahKDQy-Y*43pGM') {
+      if (req.body.senha == 'TuWztyqQbrSHb89Nc6LC6P4H') {
       fs.readdir("./data", (err, paths) => {
         var txt="";
         var arquivos = paths;
